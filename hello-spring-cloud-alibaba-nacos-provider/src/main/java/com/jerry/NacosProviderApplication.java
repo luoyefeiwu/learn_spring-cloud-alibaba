3e71ff4248bd2d3519d5c2bfc8f23e6e69dc2e60
huangchuanwang <568384493@qq.com>
2020-09-07 17:50:28 +0800

    alibaba

---
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/jerry/NacosProviderApplication.java
@@ -0,0 +1,29 @@
+package com.jerry;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+    @Value("${server.port}")
+    private String port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable String message) {
+            return "Hello Nacos Discovery " + message + " i am from port " + port;
+        }
+    }
+}
