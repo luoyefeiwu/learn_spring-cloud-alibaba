37a1f74eaac99f8ff4d7d808c3eadc5e9a91bc1a
huangchuanwang <568384493@qq.com>
2020-09-07 18:26:10 +0800

    熔断

---
+++ hello-spring-cloud-alibaba-nacos-consumer-feign/src/main/java/com/jerry/service/EchoServiceFallback.java
@@ -0,0 +1,11 @@
+package com.jerry.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EchoServiceFallback implements EchoService {
+    @Override
+    public String echo(String message) {
+        return "echo fallback";
+    }
+}
